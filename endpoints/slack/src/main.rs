58110d45418a6d45a0f427d0d653e3c07933b66d
George Hosu <george.hosu@protonmail.com>
2018-04-08 15:22:37 +0100

    temporary

--- endpoints/slack/src/main.rs
+++ endpoints/slack/src/main.rs
@@ -3,14 +3,17 @@ extern crate hyper;
 extern crate hyper_tls;
 extern crate tokio_core;
 extern crate shared_lib;
+extern crate serde_json;
+#[macro_use] extern crate serde_derive;
 
 use futures::{Future, Stream};
 use tokio_core::reactor::Core;
 use hyper::Client;
 use hyper::{Method, Request};
 use hyper::header::{ContentLength, ContentType};
+use hyper::Uri;
 use std::{thread, time};
-use std::vec::Vector;
+use std::vec::Vec;
 use shared_lib::{current_ts, read_cfg};
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
@@ -29,12 +32,13 @@ struct Receptor {
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 struct Config {
 	receptor:	Receptor,
-	monitor:	Vector<Check>
+	monitor:	Vec<Check>
 }
 
 
 fn main() {
 	let mut core = Core::new().expect("Can't start the tokio core !");
+	let mut handle1 = core.handle();
 
 	let client = Client::configure()
 		.connector(::hyper_tls::HttpsConnector::new(4, &core.handle()).unwrap())
@@ -42,36 +46,40 @@ fn main() {
 
 	let my_endpoint = "your_app/enpoint/w.e you call it add in config later";
 
-	let cfg = read_cfg::<Config>("config.yml")?;
+	let cfg = read_cfg::<Config>("config.yml").expect("Can't find config.yml file");
 
 	let receptor_uri_base = format!("{}:{}", cfg.receptor.host, cfg.receptor.port);
-	let slack_uri = format!("https://hooks.slack.com/services/{}", my_endpoint).parse().expect("Can't parse url");
+	let slack_uri: Uri = format!("https://hooks.slack.com/services/{}", my_endpoint).parse().expect("Can't parse url");
 
 	loop {
 		thread::sleep(time::Duration::from_millis(1000));
 
-		for &check in monitor {
-			let receptor_uri = format!("{}?level={}&name={}&ts_start=0&ts_end=1823146529", receptor_uri_base, check.level, check.name);
+		for check in &cfg.monitor {
+			let receptor_uri: Uri = format!("{}?level={}&name={}&ts_start=0&ts_end=1823146529",
+			receptor_uri_base, &check.level, &check.plugin).parse().expect("Can't parse url");
 
-			let mut rreq = Request::new(Method::Get, &receptor_uri);
-			let get = client.request(sreq).and_then(|rres| {
-				let message = rres.body().concat2()
+			println!("1");
+			let get = client.get(receptor_uri).map(|rres| {
+				println!("2");
+				let message = rres.body().concat2();
 
 				let json = format!("{:?}", &message);
 
-				let mut sreq = Request::new(Method::Post, &slack_uri);
+				let mut sreq = Request::new(Method::Post, slack_uri.clone());
 				sreq.headers_mut().set(ContentType::json());
 				sreq.headers_mut().set(ContentLength(json.len() as u64));
+
+				println!("Got from receptor: {}", &json);
+
 				sreq.set_body(json);
 
 				let post = client.request(sreq).and_then(|res| {
 					println!("POST: {}", res.status());
 					res.body().concat2()
 				});
-
+				//handle1.spawn(post).expect("Can't run post to slack endpoint");
 			});
-
-			core.run(get).expect("Can't get data from receptor");
+			core.run(get).expect("Can't run get to receptor");
 		}
 	}
 }
